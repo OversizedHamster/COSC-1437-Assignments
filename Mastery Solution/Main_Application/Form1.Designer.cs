8a6ac3c117303dfe0989b1f33918f0c3a01a1c4d
Ethan Smith <ethansmith1@my.lonestar.edu>
2020-09-23 20:50:56 -0500

    Conclusion of the Master Project Unit 1

---
+++ Mastery Solution/Main_Application/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Main_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.exitApplicationBtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exitApplicationBtn
+            // 
+            this.exitApplicationBtn.Location = new System.Drawing.Point(676, 407);
+            this.exitApplicationBtn.Name = "exitApplicationBtn";
+            this.exitApplicationBtn.Size = new System.Drawing.Size(112, 31);
+            this.exitApplicationBtn.TabIndex = 0;
+            this.exitApplicationBtn.Text = "Exit Application";
+            this.exitApplicationBtn.UseVisualStyleBackColor = true;
+            this.exitApplicationBtn.Click += new System.EventHandler(this.exitApplicationBtn_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(832, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(86, 20);
+            this.toolStripMenuItem1.Text = "Place Holder";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuItem2.Text = "Help";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(477, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(311, 210);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 462);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.exitApplicationBtn);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Master Assignment - Ethan Smith";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitApplicationBtn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Mastery Solution/Main_Application/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace Main_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap bitmap = (Bitmap)pictureBox1.Image;
+            pictureBox1.Image = (Image)(RotateImg(bitmap, 270.0f));
+        }
+
+        private void exitApplicationBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        public static Bitmap RotateImg(Bitmap bmp, float angle)
+
+        {
+
+            int w = bmp.Width;
+
+            int h = bmp.Height;
+
+            Bitmap tempImg = new Bitmap(w, h);
+
+            Graphics g = Graphics.FromImage(tempImg);
+
+            g.DrawImageUnscaled(bmp, 1, 1);
+
+            g.Dispose();
+
+            GraphicsPath path = new GraphicsPath();
+
+            path.AddRectangle(new RectangleF(0f, 0f, w, h));
+
+            Matrix mtrx = new Matrix();
+
+            mtrx.Rotate(angle);
+
+            RectangleF rct = path.GetBounds(mtrx);
+
+            Bitmap newImg = new Bitmap(Convert.ToInt32(rct.Width), Convert.ToInt32(rct.Height));
+
+            g = Graphics.FromImage(newImg);
+
+            g.TranslateTransform(-rct.X, -rct.Y);
+
+            g.RotateTransform(angle);
+
+            g.InterpolationMode = InterpolationMode.HighQualityBilinear;
+
+            g.DrawImageUnscaled(tempImg, 0, 0);
+
+            g.Dispose();
+
+            tempImg.Dispose();
+
+            return newImg;
+
+        }
+    }
+}
