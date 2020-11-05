e26b170769c344d725faa9a4332ce52b020cb85a
Ethan Smith <ethansmith1@my.lonestar.edu>
2020-11-04 18:38:25 -0600

    Finished unit 7 weekly topic.

---
+++ Weekly Topic Unit 7/MyGenericQueue/MyQueue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+//Ethan Smith
+
+namespace MyGenericQueue
+{
+    public class MyQueue<T> : System.Collections.Generic.IEnumerable<T>
+    {
+        private System.Collections.Generic.LinkedList<T> _list =
+            new System.Collections.Generic.LinkedList<T>();
+
+        public void Enqueue(T item)
+        {
+            _list.AddLast(item);
+        }
+
+        public T Dequeue()
+        {
+            if (_list.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty");
+            }
+
+            T value = _list.First.Value;
+            _list.RemoveFirst();
+
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (_list.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty");
+            }
+
+            return _list.First.Value;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _list.Count;
+            }
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public System.Collections.Generic.IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+    }
+}
---
+++ Weekly Topic Unit 7/MyTestsForQueues/MyGenericQueueIntegerTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+//Ethan Smith
+
+namespace MyTestsForQueues
+{
+    [TestClass]
+    public class MyGenericQueueIntegerTest
+    {
+        [TestMethod]
+        public void Create_Queue_And_Verify_Empty()
+        {
+            // assign
+            var myQueue = new MyGenericQueue.MyQueue<int>();
+
+            // act
+
+            // assert
+            myQueue.Count.ShouldBe(0);
+
+            Should.Throw<InvalidOperationException>(
+                () => myQueue.Dequeue()
+                );
+
+            Should.Throw<InvalidOperationException>(
+                () => myQueue.Peek()
+            );
+        }
+
+        [TestMethod]
+        public void Queue_And_Dequeue_Values()
+        {
+            // assign
+            var myQueue = new MyGenericQueue.MyQueue<int>();
+
+            // act
+            myQueue.Enqueue(111);
+            myQueue.Enqueue(222);
+            myQueue.Enqueue(333);
+            myQueue.Enqueue(444);
+            myQueue.Enqueue(555);
+
+            // assert
+            myQueue.Count.ShouldBe(5);
+
+            var scratch1 = myQueue.Peek();
+            scratch1.ShouldBe(111);
+
+            var scratch2 = myQueue.Dequeue();
+            scratch2.ShouldBe(111);
+
+            var scratch3 = myQueue.Dequeue();
+            scratch3.ShouldBe(222);
+
+            myQueue.Dequeue();
+            myQueue.Dequeue();
+
+            myQueue.Count.ShouldBe(1);
+
+            var scratch4 = myQueue.Dequeue();
+            scratch4.ShouldBe(555);
+
+            myQueue.Count.ShouldBe(0);
+        }
+
+        //didn't have to do anything special to make this work, just had to reference System.Linq
+        [TestMethod]
+        public void ReturnAnArray()
+        {
+            // assign
+            var myQueue = new MyGenericQueue.MyQueue<int>();
+
+            // act
+            myQueue.Enqueue(111);
+            myQueue.Enqueue(222);
+            myQueue.Enqueue(333);
+            myQueue.Enqueue(444);
+            myQueue.Enqueue(555);
+
+            // assert
+            myQueue.Count.ShouldBe(5);
+
+            var myQueueArray = myQueue.ToArray();
+            myQueueArray.ShouldBeOfType<int[]>();
+            myQueueArray[0].ShouldBe(111);
+            myQueueArray[1].ShouldBe(222);
+            myQueueArray[2].ShouldBe(333);
+            myQueueArray[3].ShouldBe(444);
+            myQueueArray[4].ShouldBe(555);
+        }
+    }
+}
